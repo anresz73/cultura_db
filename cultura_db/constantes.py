1b6f8850f2ccf33a7fffe3e50eb51fc304406d9c
anresz73 <andresreisz@gmail.com>
2022-08-11 13:32:41 -0300

    Complete procesamiento_datos

--- cultura_db/constantes.py
+++ cultura_db/constantes.py
@@ -1,16 +1,14 @@
-#
+# Variables y constantes aplicadas en las distintas funciones y métodos
 
+# Diccionario con las categorías y urls de los archivos csv a descargar y procesar.
 csv_urls = {
     'museos' : r'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv',
     'cines' : r'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv',
     'bibliotecas' : r'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv' 
     }
 
-CATEGORIA_CINES = 'cines'
-COLUMNA_AGRUPAR = 'provincia'
-
-AGGREGATOR_DICT = {'pantallas' : 'sum', 'butacas' : 'sum', 'espacio_incaa' : 'count'}
-
+# Lectura csv general
+# List - Nombre de las columnas ordenadas para pasar al df 
 COLUMNAS = [
     'cod_localidad',
     'id_provincia',
@@ -26,6 +24,7 @@ COLUMNAS = [
     'web',
     ]
 
+# Diccionario para renombrar las columnas que difieren en los tres csvs
 COLUMNAS_RENAMER = {
     'cod_loc' : 'cod_localidad',
     'idprovincia' : 'id_provincia',
@@ -38,6 +37,7 @@ COLUMNAS_RENAMER = {
     'cod_tel' : 'cod_area'
     }
 
+# Diccionario con los tipos de cada columna usada
 DTYPES_DICT = {
     'cod_localidad' : 'str',
     'id_provincia' : 'int',
@@ -54,3 +54,31 @@ DTYPES_DICT = {
     'mail' : 'str',
     'web' : 'str',
 }
+
+# Valores para confirgurar el filtrado y limpieza de la información provenientes de los archivos csv
+# Valores a considerar como NaN.
+NA_VALUES = ['s/d']
+# Dict para customizar con función lambda anónima la columna de espacio_INCAA
+CONVERTERS = {'espacio_INCAA' : lambda x: True if x.casefold() == 'si' else False}
+# Columnas a unir, teléfonos y direcciones o domicilios
+COLUMNA_MERGE = {
+    'domicilio' : ['domicilio', 'piso'],
+    'telefono' : ['cod_area', 'telefono']
+}
+# Filtrado de acentos
+TRANSLATE = str.maketrans('áéíóú', 'aeiou')
+# Nombre columna fuentes
+CATEGORIA_FUENTE = 'fuente'
+
+# Tabla Nro 3.
+# Categoría en type str de los cines
+CATEGORIA_CINES = 'cines'
+CATEGORIA_PROVINCIA = 'provincia'
+CATEGORIA_CATEGORIAS = 'categoria'
+# Columna para ser procesada y contar y sumar pantallas, butacas y espacio_incaa
+COLUMNA_AGRUPAR = 'provincia'
+# Diccionario que indica que función se aplica en cada caso, sum o count
+AGGREGATOR_DICT = {'pantallas' : 'sum', 'butacas' : 'sum', 'espacio_incaa' : 'sum'}
+#
+CANTIDAD_REGISTROS = 'cantidad_registros'
+
--- cultura_db/funciones/procesamiento_datos.py
+++ cultura_db/funciones/procesamiento_datos.py
@@ -4,6 +4,7 @@
 # Finalmente arma tres dataframes para poder ser exportados luegos a las tablas SQL.
 
 import pandas as pd
+from os.path import basename
 from ..constantes import *
 from .funciones_auxiliares import _path_file
 
@@ -16,29 +17,60 @@ def procesamiento_datos(list_categorias):
     Out:
     Tupla con los tres DataFrame
     """
+    # Loop for general para leer cada csv y aplicar filtrado y reordenamiento en los DataFrames 
+    # Lista para agregar los tres dfs.
     df_list = []
     for categoria in list_categorias:
+        # Lectura del csv
+        filepath = _path_file(categoria)
         df = pd.read_csv(
-            _path_file(categoria),
-            na_values = ['s/d'],
-            converters = {'espacio_INCAA' : lambda x: True if x.casefold() == 'si' else False},
+            # uso funcion auxiliar de armado de ruta y nombre archivo
+            filepath,
+            # valores NaN
+            na_values = NA_VALUES,
+            # Funciones para aplicar en determinadas columnas
+            converters = CONVERTERS,
             )
+        # Limpieza y normalización de columnas
         df.columns = df.columns.str.casefold()
+        df.columns = df.columns.map(lambda x: x.translate(TRANSLATE))
         df = df.rename(columns = COLUMNAS_RENAMER)
+        # Normalizar columnas específicas - Provincias algunas con y otras sin acentos
+        df[CATEGORIA_PROVINCIA] = df[CATEGORIA_PROVINCIA].map(lambda x: x.translate(TRANSLATE)).str.casefold()
+        # Asignación de tipos específicos
         df = df.astype(DTYPES_DICT)
-        df['domicilio'] = df[['domicilio', 'piso']].apply(' '.join, axis = 1)
-        df['telefono'] = df[['cod_area', 'telefono']].apply(' '.join, axis = 1)
+        # Loop for para unir las columnas indicadas (teléfono, dirección o domicilio)
+        for key, value in COLUMNA_MERGE.items():
+            df[key] = df[value].apply(' '.join, axis = 1)
+            #df['telefono'] = df[['cod_area', 'telefono']].apply(' '.join, axis = 1)
+        # Agrega columna con la fuente (nombre del archivo csv)
+        df[CATEGORIA_FUENTE] = basename(filepath)
+        # Se agrega el dataframe a la lista
         df_list.append(df)
 
+    # Diccionario con categorias como claves y los dataframes como valores
     dict_result = {k : v for k, v in zip(list_categorias, df_list)}
-
+    
+    # Tabla 1
+    # Uso de concat para armar la tabla unificada con las columnas seleccionadas
     result_1 = pd.concat(
         objs = df_list,
         axis = 0
-        ).loc[:, COLUMNAS]
+        ).loc[:, COLUMNAS + [CATEGORIA_FUENTE]]
     
-    result_2 = None
+    # Tabla 2
+    result_2 = []
+    for categoria_registro in [CATEGORIA_FUENTE, CATEGORIA_CATEGORIAS]:
+        result_2.append(result_1.groupby(categoria_registro).size())
+    #
+    df_aux = result_1.groupby([CATEGORIA_PROVINCIA, CATEGORIA_CATEGORIAS], as_index = True).size()
+    idx = [f'{e[0]}-{e[1]}' for e in df_aux.index]
+    df_aux.index = idx
+    result_2.append(df_aux)
+    result_2 = pd.concat(result_2, axis = 0).to_frame(CANTIDAD_REGISTROS)
 
+    # Tabla 3
+    # Uso de groupby con el diccionario en la categoría cine y la función agregada definida con valores a contar y sumar
     result_3 = dict_result[CATEGORIA_CINES].groupby(dict_result[CATEGORIA_CINES][COLUMNA_AGRUPAR]).agg(
         AGGREGATOR_DICT)
 
