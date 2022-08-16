804701a401d8f692a25d6c10be9f07b56edfbdfb
anresz73 <andresreisz@gmail.com>
2022-08-15 23:04:45 -0300

    Add exceptions.py for custom error handling

---
+++ cultura_db/exceptions.py
@@ -0,0 +1,13 @@
+# Excepciones customizadas para cada módulo
+
+class ArchivosFuenteException(Exception):
+    pass
+
+class DbException(Exception):
+    pass
+
+class ProcesamientoDatosException(Exception):
+    pass
+
+class DemoException(Exception):
+    pass
