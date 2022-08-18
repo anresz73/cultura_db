3582698616106840a44857f85413f882d210dacd
anresz73 <andresreisz@gmail.com>
2022-08-18 16:23:42 -0300

    Update sql files

---
+++ cultura_db/sql/constraint.sql
@@ -0,0 +1,8 @@
+CONSTRAINT FK_tabla_3_id FOREIGN KEY (id_provincia)
+    REFERENCES tabla_3 (id_provincia);
+
+FOREIGN KEY (id_provincia) REFERENCES tabla_3 (id_provincia)
+
+DROP TABLE IF EXISTS tabla_3;
+
+DROP TABLE IF EXISTS tabla_3;
--- cultura_db/sql/tabla_3.sql
+++ cultura_db/sql/tabla_3.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS tabla_3;
 CREATE TABLE tabla_3 (
+    id_provincia INTEGER PRIMARY KEY,
     provincia VARCHAR(80),
     pantallas INTEGER,
     butacas INTEGER,
