3d70d08b52acb1360bba65f391d26fcde93bd443
anresz73 <andresreisz@gmail.com>
2022-08-18 14:07:16 -0300

    Reorder functions in creacion_tables

--- cultura_db/funciones/creacion_tablas.py
+++ cultura_db/funciones/creacion_tablas.py
@@ -1,16 +1,19 @@
 # Funciones para creación de las tablas en la base de datos
 
-from ..settings import *
-from ..constantes import csv_urls
-from .procesamiento_datos import procesamiento_datos
-from .funciones_auxiliares import _sql_file_path
 import pandas as pd
 import logging
 from sqlalchemy import create_engine, text
 from sqlalchemy_utils import database_exists, create_database
 from os.path import basename
+from ..settings import *
+from ..constantes import csv_urls
+from .procesamiento_datos import procesamiento_datos
+from .funciones_auxiliares import _sql_file_path
 
-# Funciones
+################################################################
+# Funciones usadas en la clase Cultura_DB
+################################################################
+
 def get_engine(db_name, db_user, db_password, db_host, db_port):
     """
     Función para conectarse a la base de datos Postgresql con los parámetros pasados
@@ -33,40 +36,16 @@ def get_engine(db_name, db_user, db_password, db_host, db_port):
     db_engine = create_engine(db_string)
     return db_engine
 
-def get_engine_with_settings():
+def read_sql_file(sql_file_path):
     """
-    Función que devuelve el engine con las settings de las variables de settings.py
+    Función que lee y devuelve archivo .sql
+    Args:
+        sql_file_path (str): path del archivo sql
     """
-    return get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
-
-def _write_table():
-    """
-    Escribe las tablas en la base de datos
-    """
-    _engine = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
-    _file_name = r'./cultura_db/sql/tabla_1.sql'
-    _stmt = read_sql_file(_file_name)
-    db_execute(sql_stmt = _stmt, sql_engine = _engine)
-
-def drop_and_create_table():
-
-    _file_name = r'./cultura_db/sql/tabla_1.sql'
-    _engine = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
-    connection, cursor = None, None
-    connection = _engine.raw_connection()
-    try:
-        cursor = connection.cursor()
-        #print('DROP TABLE IF EXISTS %s'.format(table_name))
-        #cursor.execute("""DROP TABLE IF EXISTS %s""", table_name)
-        cursor.execute(read_sql_file(sql_file_path = _file_name))
-        connection.commit()
-    except Exception as e:
-        print(f'Error: {e}')
-    finally:
-        if cursor is not None:
-            cursor.close()
-        if connection is not None:
-            connection.close()
+    logging.info(f'Leyendo sql: {sql_file_path}')
+    with open(sql_file_path) as file:
+        sql_query = file.read()
+    return sql_query
 
 # Funciones que crean tablas y las escriben
 def db_execute(sql_stmt, sql_engine):
@@ -79,17 +58,6 @@ def db_execute(sql_stmt, sql_engine):
     #t = text(text = sql_stmt)
     with sql_engine.connect() as connection:
         connection.execute(sql_stmt)
-
-def read_sql_file(sql_file_path):
-    """
-    Función que lee y devuelve archivo .sql
-    Args:
-        sql_file_path (str): path del archivo sql
-    """
-    logging.info(f'Leyendo sql: {sql_file_path}')
-    with open(sql_file_path) as file:
-        sql_query = file.read()
-    return sql_query
 
 def _drop_and_create_table(sql_file_path, engine):
     """
@@ -125,6 +93,33 @@ def _write_table_from_df(table_name, df_tabla, engine):
                     index = False
     )
 
+
+################################################################
+# Funciones de prueba no implementadas por la clase CulturaDB
+################################################################
+
+def drop_and_create_table():
+    """
+    Función que usa raw_conexion, cursor y sql queries directas para re
+    """
+    _file_name = r'./cultura_db/sql/tabla_1.sql'
+    _engine = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
+    connection, cursor = None, None
+    connection = _engine.raw_connection()
+    try:
+        cursor = connection.cursor()
+        #print('DROP TABLE IF EXISTS %s'.format(table_name))
+        #cursor.execute("""DROP TABLE IF EXISTS %s""", table_name)
+        cursor.execute(read_sql_file(sql_file_path = _file_name))
+        connection.commit()
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None:
+            connection.close()
+
 def write_table(table_name, df_tabla, engine):
     """
     Funcion que escribe las tablas en la base de datos.
@@ -145,9 +140,18 @@ def write_table(table_name, df_tabla, engine):
         engine = engine
     )
 
+def _write_table():
+    """
+    Escribe las tablas en la base de datos
+    """
+    _engine = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
+    _file_name = r'./cultura_db/sql/tabla_1.sql'
+    _stmt = read_sql_file(_file_name)
+    db_execute(sql_stmt = _stmt, sql_engine = _engine)
+
 def write_tables(dict_tablas, _engine):
     """
-    Función que escribe las tres tablas
+    Función que escribe las tres tablas. Implementado directamente en la clase CulturaDB
     Args:
         dict_tablas (dict) : Diccionario con las tablas provenientes de la función procesamiento de datos
     """
@@ -161,7 +165,16 @@ def write_tables(dict_tablas, _engine):
             )
 
 def read_table():
-    return pd.read_sql(sql = 'SELECT * FROM tabla_1;',
-                        con = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT),
+    """
+    Ejecuta sentencia sql 
+    """
+    return pd.read_sql(
+        sql = 'SELECT * FROM tabla_1;',
+        con = get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT),
+        )
 
-                )
+def get_engine_with_settings():
+    """
+    Función que devuelve el engine con las settings de las variables de settings.py
+    """
+    return get_engine(db_name = DB_NAME, db_user = DB_USER, db_password = DB_PASSWORD, db_host = DB_HOST, db_port = DB_PORT)
