1315d19fc735aee98162583be13778b92e8bbecb
anresz73 <andresreisz@gmail.com>
2022-08-19 20:41:00 -0300

    Fix packages

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = 'cultura_db',
     version = '1.0.0',
-    url = 'https://github.com/anresz73/cultura_db.git',
+    url = 'https://github.com/anresz73/cultura_db',
+    download_url = 'https://github.com/anresz73/cultura_db',
     description = 'Proyecto que trae datos desde archivos sql, procesa y construye base de datos Postgresql.',
-    packages = find_packages(),
+    packages = [
+        'cultura_db', 
+        'cultura_db/funciones',
+        'cultura_db/sql',
+    ],
     #install_requires = []
 )
